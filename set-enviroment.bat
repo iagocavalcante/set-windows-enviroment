53499f11ee6c49daa81ff753a2543035d6650c3b
Iago Cavalcante <iagoangelimc@gmail.com>
2017-09-25 18:02:25 -0300

    File to be executed

---
+++ set-enviroment.bat
@@ -0,0 +1,133 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install jre8 -y
+
+choco install firefox -y 
+
+choco install sublimetext3 -y
+
+choco install adobereader -y
+
+choco install python -y
+
+choco install mysql.workbench -y
+
+choco install visualstudiocode -y
+
+choco install teamviewer -y
+
+choco install cmder -y
+
+choco install curl -y
+
+choco install putty -y
+
+choco install ruby -y 
+
+choco install php -y
+
+choco install nodejs.install -y 
+
+choco install libreoffice -y 
+
+choco install wget -y 
+
+choco install virtualbox -y 
+
+choco install googlechrome -y 
+
+choco install 7zip.install -y 
+
+choco install skype -y 
+
+choco install git.install -y 
+
+choco install ccleaner -y 
+
+choco install youtube-dl -y 
+
+choco install android-sdk -y 
+
+choco install sqlite -y 
+
+choco install yarn -y 
+
+choco install sql-server-management-studio -y 
+
+choco install maven -y 
+
+choco install steam -y 
+
+choco install pgadmin3 -y 
+
+choco install spotify -y 
+
+choco install docker -y
+
+choco install visualstudio2015community -y
+
+choco install mpc-hc -y
+
+choco install k-litecodecpackfull -y 
+
+choco install pycharm-community -y 
+
+choco install cpu-z.install -y
+
+choco install slack -y 
+
+choco install r.project -y
+
+choco install gradle -y 
+
+choco install mongodb -y 
+
+choco install rufus -y 
+
+choco install telegram.install -y 
+
+choco install androidstudio -y
+
+choco install geforce-experience -y
+
+choco install unity -y 
+
+choco install kindle -y 
+
+choco install docker-compose -y
+
+choco install composer -y
+
+choco install selenium-chrome-driver -y 
+
+choco install nginx -y
+
+choco install dbeaver -y 
+
+choco install whatsapp -y
+
+choco install heidisql -y 
+
+choco install arduino -y
+
+choco install electron -y
+
+choco install heroku-cli -y
+
+choco install phpstorm -y
+
+choco install obs-studio -y
+
+choco install mariadb -y
+
+choco install postman -y 
+
+choco install adobe-creative-cloud -y
+
+choco install webtorrent-desktop -y
+
+npm install -g ionic cordova bower grunt
+
+composer global require "laravel/installer"
+
+composer global require "laravel/lumen-installer"
